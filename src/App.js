f4ea265f45ff5a4779a1ef8008ab9ee39aba65f4
maximiliano <maxil5gomez@gmail.com>
2022-04-15 22:51:06 -0300

    iniciando proyecto de calculadora

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    console.log('Renderizacion de la app')
+    return ( 
+        <main className='react-calculator'> 
+            <div className='result'>
+            </div>
+            <div className='numbers'>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>0</button>
+            </div>
+            <div className='functions'>
+                <button>clear</button>
+                <button>r</button>
+            </div>
+            <div className='math-operations'>
+                <button>+</button>
+                <button>-</button>
+                <button>*</button>
+                <button>/</button>
+                <button>=</button>
+            </div>
+        </main>
+     );
 }
-
+ 
 export default App;
+ 
